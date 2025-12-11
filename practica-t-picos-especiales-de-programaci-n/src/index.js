eea2b4d43c44b0361410392407bd6ce6156dbd83
vanlendrea <vanlendrea01@gmail.com>
2025-12-11 19:35:50 -0400

    update
    final

--- practica-t-picos-especiales-de-programaci-n/src/index.js
+++ practica-t-picos-especiales-de-programaci-n/src/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import dotenv from "dotenv "
-import mongoose from ' mongoose '
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import { User } from './user.js'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 dotenv.config()
 const connectDB = () => {
   const {
@@ -32,6 +35,48 @@ app.listen(port, function () {
     console.log(`Api corriendo en http://localhost:${port}!`)
 })
 app.get('/', (req, res) => {
-    res.get('Mi primer endpoint con Express.js')
+    res.get('Mi primer endpoint')
            res.status(200).send('Hola la API está funcionando correctamente');
 });       
+
+app.post('/', async (req, res) => {
+    try {
+        var data = req.body
+        
+        var newUser = new User(data)
+
+        await newUser.save()
+        res.status(200).send({
+            success: true,
+            message: "Se registró el usuario",
+            outcome: []
+        })
+    }
+    catch (err) {
+        // Mensaje de error por si no se pudo registrar el usuario
+        res.status(400).send({
+            success: false,
+            message: "Error al intentar crear el usuario, por favor intente nuevamente",
+            outcome: []
+        })
+    }
+})
+
+app.get('/usuarios', async (req, res) => {
+    try {
+        var usuarios = await User.find().exec()
+
+        res.status(200).send({
+            success: true,
+            message: "Se encontraron los usuarios exitosamente",
+            outcome: [usuarios]
+        })
+    }
+    catch (err) {
+        res.status(400).send({
+            success: false,
+            message: "Error al intentar obtener los usuarios, por favor intente nuevamente",
+            outcome: []
+        })
+    }
+})
